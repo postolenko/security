63b8feba540a8864e44386d27c0061cb65435cf2
postolenko <denis.postolenko@gmail.com>
2015-10-28 16:07:08 +0200

    Security

--- js/scripts.js
+++ js/scripts.js
@@ -62,58 +62,58 @@ $(document).ready(function() {
 
 
 
-	if( document.getElementsByClassName("map-box")[0] ) {
+	// if( document.getElementsByClassName("map-box")[0] ) {
 
-			var map;
+	// 		var map;
 
-			var marker;
-			var image = 'img/marker.png';
+	// 		var marker;
+	// 		var image = 'img/marker.png';
 
-			var styles;
-
-
-			function initMap() {
-
-				var styles = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}];
-
-				var styledMap = new google.maps.StyledMapType(styles,
-				{name: "Styled Map"});
-
-				map = new google.maps.Map(document.getElementById('map'), {
-					center: {lat: 55.7887, lng: 49.1157},
-					scrollwheel: false,
-					scaleControl: false,
-					zoom: 15
-				});
-
-				marker = new google.maps.Marker({
-					map: map,
-					draggable: true,
-					animation: google.maps.Animation.DROP,
-					position: {lat: 55.7884, lng: 49.1154},
-					map: map,
-					icon: image,
-					title: 'Империя'
-				});
-
-				marker.addListener('click', toggleBounce);
-
-				//Associate the styled map with the MapTypeId and set it to display.
-				map.mapTypes.set('map_style', styledMap);
-				map.setMapTypeId('map_style');
+	// 		var styles;
 
 
-			}
+	// 		function initMap() {
 
-				function toggleBounce() {
-				  if (marker.getAnimation() !== null) {
-				    marker.setAnimation(null);
-				  } else {
-				    marker.setAnimation(google.maps.Animation.BOUNCE);
-				  }
-				}
+	// 			var styles = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}];
 
-		}
+	// 			var styledMap = new google.maps.StyledMapType(styles,
+	// 			{name: "Styled Map"});
+
+	// 			map = new google.maps.Map(document.getElementById('map'), {
+	// 				center: {lat: 55.7887, lng: 49.1157},
+	// 				scrollwheel: false,
+	// 				scaleControl: false,
+	// 				zoom: 15
+	// 			});
+
+	// 			marker = new google.maps.Marker({
+	// 				map: map,
+	// 				draggable: true,
+	// 				animation: google.maps.Animation.DROP,
+	// 				position: {lat: 55.7884, lng: 49.1154},
+	// 				map: map,
+	// 				icon: image,
+	// 				title: 'Империя'
+	// 			});
+
+	// 			marker.addListener('click', toggleBounce);
+
+	// 			//Associate the styled map with the MapTypeId and set it to display.
+	// 			map.mapTypes.set('map_style', styledMap);
+	// 			map.setMapTypeId('map_style');
+
+
+	// 		}
+
+	// 			function toggleBounce() {
+	// 			  if (marker.getAnimation() !== null) {
+	// 			    marker.setAnimation(null);
+	// 			  } else {
+	// 			    marker.setAnimation(google.maps.Animation.BOUNCE);
+	// 			  }
+	// 			}
+
+	// 	}
 
 
 
@@ -388,4 +388,62 @@ $(document).ready(function() {
 // 		}
 
 // }
+
+
+
+
+
+
+if( document.getElementsByClassName("map-box")[0] ) {
+
+			var map;
+
+			var marker;
+			var image = 'img/marker.png';
+
+			var styles;
+
+
+			function initMap() {
+
+				var styles = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}];
+
+				var styledMap = new google.maps.StyledMapType(styles,
+				{name: "Styled Map"});
+
+				map = new google.maps.Map(document.getElementById('map'), {
+					center: {lat: 55.7887, lng: 49.1157},
+					scrollwheel: false,
+					scaleControl: false,
+					zoom: 15
+				});
+
+				marker = new google.maps.Marker({
+					map: map,
+					draggable: true,
+					animation: google.maps.Animation.DROP,
+					position: {lat: 55.7884, lng: 49.1154},
+					map: map,
+					icon: image,
+					title: 'Империя'
+				});
+
+				marker.addListener('click', toggleBounce);
+
+				//Associate the styled map with the MapTypeId and set it to display.
+				map.mapTypes.set('map_style', styledMap);
+				map.setMapTypeId('map_style');
+
+
+			}
+
+				function toggleBounce() {
+				  if (marker.getAnimation() !== null) {
+				    marker.setAnimation(null);
+				  } else {
+				    marker.setAnimation(google.maps.Animation.BOUNCE);
+				  }
+				}
+
+		}
 
